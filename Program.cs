8ec8514c0d58c2b0bf216cdd0fc240b9b9eea8e9
GabrielRgz <l20211835@tectijuana.edu.mx>
2021-03-09 12:40:08 -0800

    Primer Commit
    ¡00 Mi primer commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Alumno
+    {
+        //  Campos  o   Atributos
+        public string nombre;
+        public string apellido;
+        public string correo;
+
+        public void ImprimeEnConsola()
+        {
+        Console.WriteLine("Hola soy {0} y me apellido {1} envíame un correo a {2}",nombre,apellido,correo);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+
+        Alumno ana = new Alumno();
+        ana.nombre = "Ana";
+        ana.apellido = "Li";
+        ana.correo = "ana@gmail.com";
+
+        Alumno John = new Alumno();
+        John.nombre = "John";
+        John.apellido = "Roma";
+        John.correo = "JRoma@gmail.com";
+
+        ana.ImprimeEnConsola();
+        John.ImprimeEnConsola();
+
+        Console.ReadKey();
+
+
+        }
+    }
